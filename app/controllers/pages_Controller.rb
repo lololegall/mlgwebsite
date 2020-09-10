231f7263d63a28ae55c725ed0a5b170d549bfa58
Margaux Le Gall <margauxxlegall@gmail.com>
2020-09-10 20:56:43 +0200

    home

---
+++ app/controllers/pages_Controller.rb
@@ -0,0 +1,28 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def work
+  end
+
+  def whatido
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def margalou
+  end
+
+  def sucresale
+  end
+
+  def defiwind
+  end
+
+  def unchienenville
+  end
+end
